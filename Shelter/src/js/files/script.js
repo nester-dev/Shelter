6d9e6599dc1886b00d4a1ade4f04c52f44eb4c59
Dmitriy <nester.dev@gmail.com>
2022-03-21 19:22:29 +0300

    feat: add 'Header' and 'Not only' blocks

--- Shelter/src/js/files/script.js
+++ Shelter/src/js/files/script.js
@@ -2,3 +2,8 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+export function hover() {
+	const menuLink = document.querySelectorAll(".menu__link");
+	console.log(menuLink);
+}
