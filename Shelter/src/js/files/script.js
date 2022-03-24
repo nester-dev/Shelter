266822f18dfc04f2153e88c6b62deee8de1256fe
Dmitriy <nester.dev@gmail.com>
2022-03-24 11:21:44 +0300

    feat: implement popup in the 'Our friends' block

--- Shelter/src/js/files/script.js
+++ Shelter/src/js/files/script.js
@@ -3,7 +3,52 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-export function hover() {
-	const menuLink = document.querySelectorAll(".menu__link");
-	console.log(menuLink);
+function popup() {
+	const popupLinks = document.querySelectorAll(".popup-link"),
+		popup = document.querySelector(".popup"),
+		closeIcon = document.querySelectorAll(".popup__close"),
+		body = document.querySelector("body");
+	const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + "px";
+
+
+	popupLinks.forEach((item, index) => {
+		item.addEventListener("click", function (e) {
+			popup.classList.add("popup-open");
+			bodyLock();
+			e.preventDefault();
+		});
+	});
+
+	function popupClose() {
+		popup.addEventListener('click', function (e) {
+			closeIcon.forEach(item => {
+				if (e.target == item || !e.target.closest(".popup__content")) {
+					popup.classList.remove("popup-open");
+					bodyUnlock();
+					e.preventDefault();
+				}
+			});
+		});
+	}
+
+	function bodyLock() {
+		body.style.paddingRight = lockPaddingValue;
+		body.style.overflow = 'hidden';
+		body.classList.add('lock');
+	}
+	function bodyUnlock() {
+		setTimeout(function () {
+			body.style.paddingRight = '0px';
+			body.style.overflow = 'auto';
+			body.classList.remove('lock');
+		}, 700);
+	}
+
+	popupClose();
 }
+
+
+
+
+
+popup();
